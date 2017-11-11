46932958a6f34028043b9abec2abe27263f17961
Mark Harris <mharris@technobear.com>
2017-11-11 15:16:40 +0100

    add oled info to otcweb

--- OTC_Web/run.sh
+++ OTC_Web/run.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+
+
+IP=$(ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
+oscsend localhost 4001 /oled/aux/clear i 1
+
+oscsend localhost 4001 /oled/aux/line/1 s "Started OTC Web."
+oscsend localhost 4001 /oled/aux/line/2 s "Vist here:"
+oscsend localhost 4001 /oled/aux/line/3 s "$IP:8080"
+oscsend localhost 4001 /oled/aux/line/4 s "with your browser."
+oscsend localhost 4001 /oled/aux/line/5 s " "
+
+# set to aux screen, signals screen update
+oscsend localhost 4001 /oled/setscreen i 1
+
+
 export DIR=`dirname $0`
 cd $DIR
 #wget http://www.thepeacetreaty.org/ping/ping.php
