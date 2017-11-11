f801c8532762e9911a1f6f9a03020fc7e5422858
Mark Harris <mharris@technobear.com>
2017-11-11 16:35:07 +0000

    fix for pre 2.2 OS

--- OTC_Web/run.sh
+++ OTC_Web/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-USER_DIR=${USER_DIR:="/usbdrive"}
+export USER_DIR=${USER_DIR:="/usbdrive"}
 # PATCH_DIR=${PATCH_DIR:="/usbdrive/Patches"}
 # FW_DIR=${FW_DIR:="/root"}
 # SCRIPTS_DIR=$FW_DIR/scripts
